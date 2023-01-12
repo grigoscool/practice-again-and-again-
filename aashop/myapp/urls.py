5d14330526a0b6e7f0cc65feaec5cf2b964868e2
Sergey Grigorev <grigoscool@mail.ru>
2023-01-12 19:11:01 +0300

    create more html and tag for

--- aashop/myapp/urls.py
+++ aashop/myapp/urls.py
@@ -3,6 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', show_home, name='home'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('add_item', add_item, name='add_item'),
+    path('contact', show_contact, name='contact'),
+    path('login', login, name='login'),
+
 ]
 
--- aashop/myapp/views.py
+++ aashop/myapp/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .models import *
 
-menu = ['главная', 'о сайте']
+menu = [
+    {'title': 'о сайте', 'url': 'about'},
+    {'title': 'добавть товар', 'url': 'add_item'},
+    {'title': 'обратная связь', 'url': 'contact'},
+    {'title': 'Войти', 'url': 'login'}
+]
 
 
 def show_home(request):
@@ -10,6 +15,18 @@ def show_home(request):
     return render(request, 'myapp/home.html', {'item': item, 'menu': menu})
 
 
-
 def about(request):
     return render(request, 'myapp/about.html', {'menu': menu})
+
+
+def show_contact(request):
+    return render(request, 'myapp/contact.html', {'menu': menu})
+
+
+
+def add_item(request):
+    return render(request, 'myapp/add_item.html', {'menu': menu})
+
+
+def login(request):
+    return render(request, 'myapp/login.html', {'menu': menu})
