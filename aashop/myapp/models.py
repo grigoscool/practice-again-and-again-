21b991ae48876ff0259ba10989048aadecedbcd9
Sergey Grigorev <grigoscool@mail.ru>
2023-01-12 15:59:10 +0300

    create first model - Item

--- aashop/myapp/models.py
+++ aashop/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    text = models.TextField(blank=True)
+    price = models.PositiveIntegerField(help_text='Цена товара')
+    photo = models.ImageField(null=True)
+    is_piblished = models.BooleanField(default=True)
+    time_creation = models.DateField(auto_now_add=True)
