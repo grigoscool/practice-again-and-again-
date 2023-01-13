da03b8c8c5b219d961481afb6c0b7cdda4a509e3
Sergey Grigorev <grigoscool@mail.ru>
2023-01-13 18:18:34 +0300

    add ItemAdmin

--- aashop/myapp/models.py
+++ aashop/myapp/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length=255, verbose_name='Название')
     text = models.TextField(blank=True)
     price = models.PositiveIntegerField(help_text='Цена товара')
-    photo = models.ImageField(null=True)
+    photo = models.ImageField(upload_to='photo', null=True)
     is_piblished = models.BooleanField(default=True)
     time_creation = models.DateField(auto_now_add=True)
     cat = models.ForeignKey('Category', on_delete=models.CASCADE, null=True)
