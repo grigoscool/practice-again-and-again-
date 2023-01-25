884c107e85e355ee0ba0dd0a354dfffa1f016dc5
Sergey Grigorev <grigoscool@mail.ru>
2023-01-25 16:01:17 +0300

    1

--- aashop/myapp/utils.py
+++ aashop/myapp/utils.py
@@ -15,16 +15,16 @@ class DataMixin:
     def get_user_context(self, **kwargs):
         context = kwargs
         # кэшируем запрос
-        cat = cache.get('cats')
-        if not cat:
-            cat = Category.objects.all()
-            cache.set('cat', cat, 60)
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.all()
+            cache.set('cats', cats, 60)
 
         # если пользователь не зареган, удаляем из меню добавление итема
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
             user_menu.pop(1)
 
-        context['cat'] = cat
+        context['cats'] = cats
         context['menu'] = user_menu
         return context
